2b6aba56e637897bc7d599f638fc86cb503cbe3e
emma-obelenge <obeleemmanuel@gmail.com>
2024-08-05 03:40:49 -0700

    fix all errors except circular import issue with pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+# Defining the blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *  # noqa
 from api.v1.views.states import *  # noqa
 from api.v1.views.amenities import *  # noqa
 from api.v1.views.cities import *
 from api.v1.views.users import *  # noqa
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
